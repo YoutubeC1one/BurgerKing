ad8237b5aeb9dd5c2b284693a46db922ca166c2b
Sang jin Lee <w1004123@gmail.com>
2023-06-01 10:31:28 +0900

    build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './globalStyle.css';
+import * as S from './AppCss.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <S.HeaderWrapper>
+        sdfsdf
+      </S.HeaderWrapper>
     </div>
   );
 }
