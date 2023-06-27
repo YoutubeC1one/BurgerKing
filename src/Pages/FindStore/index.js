d0aa60108e40fc9064b91b5074ac07270d3ec098
lsj0202 <w1004123@gmail.com>
2023-06-27 13:42:18 +0900

    kakao map 연동 성공

--- src/Pages/FindStore/index.js
+++ src/Pages/FindStore/index.js
@@ -1,11 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as S from "./style";
 import ShowStoreNav from "../../components/ShowStoreNav";
 
-export default function index() {
+const { kakao } = window;
+
+export default function Index() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=db30475675f4829df0478c9b3228f856";
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      kakao.maps.load(function () {
+        const container = document.getElementById("map");
+        const options = {
+          center: new kakao.maps.LatLng(37.566826, 126.9786567),
+          level: 3,
+        };
+
+        const map = new kakao.maps.Map(container, options);
+        const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+
+        const ps = new kakao.maps.services.Places();
+        ps.keywordSearch("부산버거킹", placesSearchCB);
+
+        function placesSearchCB(data, status, pagination) {
+          if (status === kakao.maps.services.Status.OK) {
+            var bounds = new kakao.maps.LatLngBounds();
+
+            for (var i = 0; i < data.length; i++) {
+              displayMarker(data[i]);
+              bounds.extend(
+                new kakao.maps.LatLng(data[i].y, data[i].x)
+              );
+            }
+
+            map.setBounds(bounds);
+          }
+        }
+
+        function displayMarker(place) {
+          var marker = new kakao.maps.Marker({
+            map: map,
+            position: new kakao.maps.LatLng(place.y, place.x),
+          });
+
+          kakao.maps.event.addListener(marker, "click", function () {
+            infowindow.setContent(
+              '<div style="padding:5px;font-size:12px;">' +
+                place.place_name +
+                "</div>"
+            );
+            infowindow.open(map, marker);
+          });
+        }
+      });
+    };
+  }, []);
+
   return (
     <>
       <ShowStoreNav />
+      <S.Container>
+        <div id="map" style={{ width: "100%", height: 520 }}></div>
+      </S.Container>
     </>
   );
 }
--- src/Pages/FindStore/style.js
+++ src/Pages/FindStore/style.js
@@ -1,36 +1,6 @@
-import styled, { keyframes, css } from "styled-components";
-
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-`;
+import styled from "styled-components";
 
 export const Container = styled.div`
-  cursor: pointer;
-  position: absolute;
-  width: 50px;
-  height: 50px;
-  background-color: silver;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 10px 30px 10px 1450px;
-  opacity: 0;
-  transition: opacity 0.3s;
-
-  ${({ show }) =>
-    show &&
-    css`
-      opacity: 1;
-      animation: ${fadeIn} 0.5s ease-in-out;
-    `}
-`;
-
-export const Image = styled.img`
-  width: 30px;
-  height: 30px;
+  width: 100%;
+  height: 74vh;
 `;
